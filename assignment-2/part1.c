ca520d96c2cbc1646d376a256890218f7f5c4615
Shahu Ronghe <sronghe1@binghamton.edu>
2021-09-27 23:47:57 -0400

    read/write done

--- assignment-2/part1.c
+++ assignment-2/part1.c
@@ -6,10 +6,19 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <regex.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024  /* make it a 1K shared memory segment */
 
 
 //function declaration.
 bool checkArgumentsForInteger(int argc, char *argv[]);
+void writeValues(int val1, int val2, int pid);
+
+
+int *data;
+int shmid;
 
 /*
  * Function: main
@@ -32,17 +41,26 @@ int main(int argc, char *argv[]){
 		printf("Please enter the number of levels of the tree (L) AND number of children of each internal node of the process tree (N).\n");
 		return -1;
 	}
-    int P = atoi(argv[1]);
+	int arr[3]= {1,2,-1};
+	int winner_id;
+
+	int P = atoi(argv[1]);
         int M = atoi(argv[2]);
         pid_t pid = -1;
+	
+	for(int i = 0; i < P; i++){
+		if(pid == -1 || pid > 0){
+			pid = fork();
+		}
+	}
 
         
-      
-      if(pid > 0){
+      	if(pid > 0){
+		//parent block
                 key_t key;
-                int shmid;
-                int *data;
-                int arr[5]={1,2};
+//                int shmid;
+  //              int *data;
+                int arr[3]={1,2,-1};
                 int Winner_id;
                 int mode;
                 int total;
@@ -54,11 +72,13 @@ int main(int argc, char *argv[]){
            }
 
            /* create the shared memory segment: */
-           if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT | IPC_EXCL )) < 0) {
-                perror("shmget");
-                exit(1);
-           }
-           
+	   if ((shmid = shmget(key, SHM_SIZE, 0644)) == -1) {
+		   if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT | IPC_EXCL )) < 0) {
+	       		   perror("shmget");
+			   exit(1);
+		   }
+    	   }
+
            /* attach to the segment to get a pointer to it: */
             data = shmat(shmid, (void *)0, 0);
             if (data == (int *)(-1)) {
@@ -66,42 +86,37 @@ int main(int argc, char *argv[]){
                 exit(1);
             }
 
-                memcpy(data,arr,sizeof(arr));
-
-            
-
-                for(int i = 0; i < P; i++){
-                if(pid == -1 || pid > 0){
-                    pid = fork();
-                }
+	    //writing initial values to shared memory
+	    writeValues(arr[0], arr[1], arr[2]);
+	    printf("values: %d , %d , %d \n",data[0],data[1],data[2]);
               
-              
-      
-                        while ((pid = waitpid(-1, &status, 0)) != -1);
-            }
+	    while ((pid = waitpid(-1, 0, 0)) != -1);
+	}
+	printf("pid: %d\n",pid);
+        if (pid == 0){
+		//child block
+		printf("executing child, %d , %d\n",data[0],data[1]);
+		while(data[0] < M || data[1] < M){
+			int total = data[0] + data[1];
+			if (data[0] < data[1])
+				data[0] = total;
+			else
+				data[1] = total;
 
-            if (pid == 0){
-
-                if(arr[0] || arr[1] < M)
-                {
-                    exit(-1);
-                }
-                else
-                {
-                    total=arr[0]+ arr[1];
-                    if(arr[0] < arr[1])
-                    {
-                        arr[0]=total;
-                    }
-                    else
-                    {
-                        arr[1]=total;
-                    }
-                }
-                
-
-            }
+			if(total > M)
+				data[2] = getpid();
+		}
+		printf("Winner PID: %d\n",data[2]);
+		exit(0);
+	}
+	printf("exited\n");
 	return 0;	
+}
+
+void writeValues(int val1, int val2, int pid){
+	data[0] = val1;
+	data[1] = val2;
+	data[2] = pid;
 }
 
 
